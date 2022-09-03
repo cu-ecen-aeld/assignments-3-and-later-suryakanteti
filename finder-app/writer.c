09251d0d5b10ff76b26e73b4d591bef8107af071
Surya Kanteti <suryateja.kanteti@gmail.com>
2022-09-03 17:39:56 -0600

    Change error return value from -1 to 1

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
 	if(argc != 3) // Including the application itself
 	{
 		// Error message using SYSLOG
-		return -1;
+		return 1;
 	}
 	
 	const char* writeFile = argv[1];
@@ -30,26 +30,26 @@ int main(int argc, char* argv[])
 	if (fd == -1)
 	{
 		// Error has occurred. Use syslog statement here
-		return -1;
+		return 1;
 	}
 	
 	ssize_t writeResult = write(fd, writeStr, strlen(writeStr));
 	if(writeResult == -1)
 	{
 		// Error message using SYSLOG. Write error. Check errno
-		return -1;
+		return 1;
 	}
 	else if(writeResult != strlen(writeStr))
 	{
 		// Error. String not correctly written
-		return -1;
+		return 1;
 	}
 	
 	fd = close(fd);
 	if(fd == -1)
 	{
 		// Error while closing the file
-		return -1;
+		return 1;
 	}
 	return 0;
 }
