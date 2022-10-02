c041faff2d5fd2764954fe4bdd96d475aa45c437
Surya Kanteti <suryateja.kanteti@gmail.com>
2022-10-01 22:10:28 -0600

    Complete code. Need to check working

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <signal.h>
 
 #define PORT_NUM_STR "9000"
 #define MAX_PENDING_CONNECTIONS 3
@@ -15,11 +16,23 @@
 #define PACKET_BUFFER_SIZE 512
 
 
+bool terminate = false;
+
+void SignalHandler(int signo)
+{
+	terminate = true;
+	syslog(LOG_INFO, "Caught signal, exiting");
+}
+
 int main()
 {	
 	int rc, serverSockfd, acceptedSocketfd;
 	struct addrinfo hints, *serverInfo, *clientInfo;
 	socklen_t* clientInfoLen;
+	
+	//Register for signals
+	signal(SIGINT, SignalHandler);
+	signal(SIGTERM, SignalHandler);
 	
 	// Set up syslog
 	openlog("aesdsocket", LOG_NDELAY, LOG_USER);
@@ -68,103 +81,110 @@ int main()
 		return -1;
 	}
 	
-	
-	// Accept the connection
-	acceptedSocketfd = accept(serverSockfd, clientInfo, clientInfoLen);
-	if(acceptedSocketfd == -1)
+	while(!terminate)
 	{
-		perror("accept");
-		freeaddrinfo(serverInfo);
-		return -1;
-	}
-	syslog(LOG_INFO, "Accept connection from %s", clientInfo->ai_addr->sa_data); // Log client IP to syslog
 	
-	
-	// Create data file if it doesn't exist. Open it
-	int fd;
-	fd = open(AESD_SOCKET_DATA_FILE, O_APPEND | O_CREAT);
-	if(fd == -1)
-	{
-		perror("socket data file open");
-		freeaddrinfo(serverInfo);
-		return -1;
-	}
-	
-	// Receive data from client and append to data file
-	
-	char* pktBuf = (char*)(malloc(PACKET_BUFFER_SIZE * sizeof(char))); // Packet buffer of 512 chars
-	ssize_t retVal;
-	char* ptr;
-	int remainingSize = 0;
-	
-	if(pktBuf == NULL)
-	{
-		printf("Error while creating packet buffer!\n");
-		freeaddrinfo(serverInfo);
-		close(fd);
-		return -1;
-	}
-	
-	memset(message, 0, strlen(message)); // Make all zeroes
-	
-	
-	while((retVal = recv(acceptedSocketfd, pktBuf, sizeof pktBuf, 0)) != 0) // Until nothing more to read
-	{
-		if(retVal == -1)
+		// Accept the connection
+		acceptedSocketfd = accept(serverSockfd, clientInfo, clientInfoLen);
+		if(acceptedSocketfd == -1)
 		{
-			perror("recv");
+			perror("accept");
 			freeaddrinfo(serverInfo);
-			close(fd);
 			return -1;
 		}
-		
-		// Check if the buffer has newline
-		ptr = strchr(pktBuf, '\n');
-		if(ptr == NULL)
+		syslog(LOG_INFO, "Accept connection from %s", clientInfo->ai_addr->sa_data); // Log client IP to syslog
+	
+	
+		// Create data file if it doesn't exist. Open it
+		int fd;
+		fd = open(AESD_SOCKET_DATA_FILE, O_APPEND | O_CREAT);
+		if(fd == -1)
 		{
-			// No newline character, extend the buffer and continue reading
-			pktBuf = (char*)(realloc(pktBuf, strlen(pktBuf) + PACKET_BUFFER_SIZE));
-			if(pktBuf == NULL)
+			perror("socket data file open");
+			close(acceptedSocketfd);
+			freeaddrinfo(serverInfo);
+			return -1;
+		}
+	
+		// Receive data from client and append to data file
+	
+		char* pktBuf = (char*)(malloc(PACKET_BUFFER_SIZE * sizeof(char))); // Packet buffer of 512 chars
+		ssize_t retVal;
+		char* ptr;
+		int remainingSize = 0;
+	
+		if(pktBuf == NULL)
+		{
+			printf("Error while creating packet buffer!\n");
+			close(fd);
+			close(acceptedSocketfd);
+			freeaddrinfo(serverInfo);
+			return -1;
+		}
+	
+		memset(message, 0, strlen(message)); // Make all zeroes
+	
+	
+		while((retVal = recv(acceptedSocketfd, pktBuf, sizeof pktBuf, 0)) != 0) // Until nothing more to read
+		{
+			if(retVal == -1)
 			{
-				printf("Error while reallocating buffer!\n");
-				freeaddrinfo(serverInfo);
+				perror("recv");
 				close(fd);
+				close(acceptedSocketfd);
+				freeaddrinfo(serverInfo);
 				return -1;
 			}
+		
+			// Check if the buffer has newline
+			ptr = strchr(pktBuf, '\n');
+			if(ptr == NULL)
+			{
+				// No newline character, extend the buffer and continue reading
+				pktBuf = (char*)(realloc(pktBuf, strlen(pktBuf) + PACKET_BUFFER_SIZE));
+				if(pktBuf == NULL)
+				{
+					printf("Error while reallocating buffer!\n");
+					close(fd);
+					close(acceptedSocketfd);
+					freeaddrinfo(serverInfo);
+					return -1;
+				}
+			}
+			else
+			{
+				// Buffer has newline character. Send to client
+				send(acceptedSocketfd, pktBuf, ptr - pktBuf);
+			
+				//Write till there into the file
+				rc = write(fd, pktBuf, (ptr - pktBuf) * sizeof(char));
+			
+				// Move rest of the content to the start of buffer
+				remainingSize = (int)(strlen(pktBuf) + pktBuf - ptr - 1);
+				memcpy(pktBuf, ptr + 1, remainingSize);
+			
+				// Clear out the remaining part
+				memset(pktBuf + remainingSize, 0, strlen(pktBuf) - remainingSize);
+			}
 		}
-		else
-		{
-			// Buffer has newline character. Send to client
-			send(acceptedSocketfd, pktBuf, ptr - pktBuf);
-			
-			//Write till there into the file
-			rc = write(fd, pktBuf, (ptr - pktBuf) * sizeof(char));
-			
-			// Move rest of the content to the start of buffer
-			remainingSize = (int)(strlen(pktBuf) + pktBuf - ptr - 1);
-			memcpy(pktBuf, ptr + 1, remainingSize);
-			
-			// Clear out the remaining part
-			memset(pktBuf + remainingSize, 0, strlen(pktBuf) - remainingSize);
-		}
+	
+		close(fd);
+		close(acceptedSocketfd);
+		syslog(LOG_INFO, "Closed connection from %s", clientInfo->ai_addr->sa_data); // Log that connection closed
+	
 	}
 	
 	
 	
-	syslog(LOG_INFO, "Closed connection from %s", clientInfo->ai_addr->sa_data); // Log that connection closed
+	// If it reached this point, file must be deleted
+	rc = remove(AESD_SOCKET_DATA_FILE);
+	if(rc == -1)
+	{
+		perror("remove socket data file");
+		freeaddrinfo(serverInfo);
+		return -1;
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	// freeaddrinfo(res)
+	freeaddrinfo(serverInfo);
 	return 0;
 }
