396ae0d9296286c9cfffc5fc1f079867cbb8ed3f
Surya Kanteti <suryateja.kanteti@gmail.com>
2022-10-02 02:27:14 -0600

    Working with sockettest.sh

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -8,34 +8,85 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 #include <signal.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
+// Macros
 #define PORT_NUM_STR "9000"
 #define MAX_PENDING_CONNECTIONS 3
 #define AESD_SOCKET_DATA_FILE "/var/tmp/aesdsocketdata"
 #define PACKET_BUFFER_SIZE 512
 
-
+// Global variables
 bool terminate = false;
+bool connectionInProgress = false;
+struct addrinfo* serverInfo = NULL;
 
+/*
+ * Function for handling registered signals
+ */
 void SignalHandler(int signo)
 {
-	terminate = true;
 	syslog(LOG_INFO, "Caught signal, exiting");
+	terminate = true;
+
+	// No active connections, exit
+	if(!connectionInProgress)
+	{
+		int rc = remove(AESD_SOCKET_DATA_FILE);
+		if(rc == -1)
+		{
+			perror("remove socket data file");
+			freeaddrinfo(serverInfo);
+			exit(-1);
+		}
+	
+		freeaddrinfo(serverInfo);
+		exit(-1);
+	}
 }
 
+/*
+ * Function to send file content through socket
+ */
+void SendFileDataToClient(int fileFd, int socketFd)
+{
+	ssize_t ret;
+	char buf[512];
+
+	// Seek to start of file
+	lseek(fileFd, 0, SEEK_SET);
+
+	while ((ret = read (fileFd, buf, sizeof buf)) != 0)
+	{
+        if (ret == -1) 
+		{
+                if (errno == EINTR)
+                    continue;
+                perror ("read");
+                break;
+        }
+
+		// Send whatever is read to client
+		send(socketFd, buf, (ret/sizeof(char)), 0);
+	}
+}
+
+/*
+ * Main: Entry point for the program
+ */
 int main()
 {	
 	int rc, serverSockfd, acceptedSocketfd;
-	struct addrinfo hints, *serverInfo;
+	struct addrinfo hints;
 	struct sockaddr clientInfo;
 	socklen_t clientInfoLen = sizeof clientInfo;
 	
 	//Register for signals
-	//signal(SIGINT, SignalHandler);
+	signal(SIGINT, SignalHandler);
 	signal(SIGTERM, SignalHandler);
 	
 	// Set up syslog
@@ -97,6 +148,7 @@ int main()
 			freeaddrinfo(serverInfo);
 			return -1;
 		}
+		connectionInProgress = true;
 		syslog(LOG_INFO, "Accept connection from %s", clientInfo.sa_data); // Log client IP to syslog
 	
 	
@@ -129,7 +181,7 @@ int main()
 	
 		memset(pktBuf, 0, currentBufSize); // Make all zeroes
 	
-		while((retVal = recv(acceptedSocketfd, pktBuf, currentBufSize, 0)) != 0) // Until nothing more to read
+		while((retVal = recv(acceptedSocketfd, pktBuf + currentBufSize - PACKET_BUFFER_SIZE, PACKET_BUFFER_SIZE, 0)) != 0) // Until nothing more to read
 		{
 			if(retVal == -1)
 			{
@@ -158,11 +210,11 @@ int main()
 			}
 			else
 			{
-				// Buffer has newline character. Send to client
-				send(acceptedSocketfd, pktBuf, ptr - pktBuf + 1, 0);
-			
-				//Write till there into the file
+				// Buffer has newline character. Write till there into the file
 				rc = write(fd, pktBuf, (ptr - pktBuf + 1) * sizeof(char));
+
+				// Send to client
+				SendFileDataToClient(fd, acceptedSocketfd);
 			
 				// Move rest of the content to the start of buffer
 				remainingSize = (int)(currentBufSize + pktBuf - ptr - 1);
@@ -175,6 +227,7 @@ int main()
 	
 		close(fd);
 		close(acceptedSocketfd);
+		connectionInProgress = false;
 		syslog(LOG_INFO, "Closed connection from %s", clientInfo.sa_data); // Log that connection closed
 	
 	}
