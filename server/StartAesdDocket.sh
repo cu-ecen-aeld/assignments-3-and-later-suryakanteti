ff3274cfc97b5a4f6d6ae075cd64de7b6d0d28fd
Surya Kanteti <suryateja.kanteti@gmail.com>
2022-10-23 23:50:52 -0600

    Move driver loading to socket script

--- server/StartAesdDocket.sh
+++ server/StartAesdDocket.sh
@@ -4,10 +4,12 @@ case "$1" in
     start)
         echo "Starting AESD Socket"
         start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+        /usr/bin/aesdchar_load
         ;;
     stop)
         echo "Stopping AESD Socket"
         start-stop-daemon -K -n aesdsocket
+        /usr/bin/aesdchar_unload
         ;;
     *)
         echo "Usage: $0 {start|stop}"
