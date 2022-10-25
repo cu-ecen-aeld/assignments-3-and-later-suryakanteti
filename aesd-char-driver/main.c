22794cc352a383a951bb116368dbd2da8d433a5b
Surya Kanteti <suryateja.kanteti@gmail.com>
2022-10-24 21:51:13 -0600

    Dummy commit

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -5,7 +5,7 @@
  * Based on the implementation of the "scull" device driver, found in
  * Linux Device Drivers example code.
  *
- * @author Dan Walkes
+ * @author Dan Walkes, edited by Guruprashanth Krishnakumar
  * @date 2019-10-22
  * @copyright Copyright (c) 2019
  *
@@ -14,13 +14,14 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/printk.h>
-#include <linux/types.h>
+#include <linux/types.h> //size_t    
 #include <linux/cdev.h>
+#include <linux/kernel.h> //containerof
+#include <linux/slab.h> //kmalloc
+#include <linux/uaccess.h>	/* copy_*_user */
 #include <linux/fs.h> // file_operations
+
 #include "aesdchar.h"
-
-#include <linux/slab.h>
-
 int aesd_major =   0; // use dynamic major
 int aesd_minor =   0;
 
@@ -29,25 +30,67 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 struct aesd_dev aesd_device;
 
+/*
+*   Allocated or reallocate the requested amount of memory
+*
+*   Args:
+*       ptr - store pointer to requested memory here
+*       size - allocated size bytes of memory
+*   Params:
+*       0 if success, -ENOMEM if failed
+*/
+static int allocate_memory(struct working_buffer *ptr, int size);
+
+static int allocate_memory(struct working_buffer *ptr, int size)
+{
+    int retval = 0;
+    char *temp_ptr;
+    if(ptr->size == 0)
+    {
+        ptr->buffer = kmalloc(size,GFP_KERNEL);
+        if(!ptr->buffer)
+        {
+            retval = -ENOMEM;
+            //Add a goto statement. We need to free COPY_BUFFER and 
+            //UNLOCK MUTEX   
+        }
+    }
+    else
+    {
+        temp_ptr = krealloc(ptr->buffer,ptr->size + size,GFP_KERNEL);
+        if(!temp_ptr)
+        {
+            kfree(ptr->buffer);
+            retval = -ENOMEM;
+            //Add a goto statement. We need to free COPY_BUFFER and WORKING_BUFFER
+            //UNLOCK MUTEX   
+            
+        }
+        else
+        {
+            ptr->buffer = temp_ptr;
+        }
+    }
+    return retval;
+}
+
 int aesd_open(struct inode *inode, struct file *filp)
 {
-    struct aesd_dev* dev;
-
+    struct aesd_dev *info_dev;
     PDEBUG("open");
-    
-    dev = container_of(inode->i_cdev, struct aesd_dev, cdev);
-    filp->private_data = dev;
-
+    //find the aesd_dev structure associated with the passed inode and assign it to private_data
+    info_dev = container_of(inode->i_cdev, struct aesd_dev, cdev);
+    filp->private_data = info_dev;
     return 0;
 }
 
 int aesd_release(struct inode *inode, struct file *filp)
 {
     PDEBUG("release");
-    
-    // De-allocate anything that open() allocated in filp->private_data
+    /**
+     * Remove assignment of private data.
+     */
     filp->private_data = NULL;
-
     return 0;
 }
 
@@ -55,140 +98,169 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
                 loff_t *f_pos)
 {
     ssize_t retval = 0;
-    size_t currentOffset; // Offset in working entry
-    size_t bytesToRead;
-    struct aesd_dev* dev;
-    struct aesd_buffer_entry* currentReadEntry;
-
+    struct aesd_dev *data;
+    size_t relative_byte_offset;
+    struct aesd_buffer_entry *ret_buf;
+    int bytes_to_eob;
+    int bytes_to_copy;
     PDEBUG("read %zu bytes with offset %lld",count,*f_pos);
-    PDEBUG("Proceed to reading!");
-
-    // Initial checks
-    if(filp == NULL || buf == NULL || f_pos == NULL)
+    if(!filp || !filp->private_data)
     {
         retval = -EINVAL;
-        goto READ_RET;
+        goto ret_func;
     }
-
-    // Fetch the aesd_dev structure
-    dev = filp->private_data;
-
-    // Lock the data using a mutex
-    if(mutex_lock_interruptible(&(dev->mut)))
+    data = filp->private_data;
+    //block on mutex lock, can be interrupted by signal
+    if (mutex_lock_interruptible(&data->lock))
     {
         retval = -EINTR;
-        goto READ_RET;
+        
+        //Add a goto statement to the end
+        goto ret_func;
     }
-
-    // First go to location pointed by f_pos and get current entry, current offset
-    currentReadEntry = aesd_circular_buffer_find_entry_offset_for_fpos(&(dev->circularBuffer), *f_pos, &currentOffset);
-    if(currentReadEntry == NULL)
+    //find data at an offsert fpos
+    ret_buf = aesd_circular_buffer_find_entry_offset_for_fpos(&data->circular_buffer,
+                                                            *f_pos, 
+                                                            &relative_byte_offset);
+    if(!ret_buf)
     {
-        goto MUTEX_UNLOCK; // Invalid offset or no more bytes to be read
+        //free MUTEX and exit with retval as 0 (EOF reached)
+        *f_pos = 0;
+        goto free_lock;
     }
-
-    // Check how many bytes to read from buffer
-    if(currentReadEntry->size - currentOffset >= count)
+    //bytes to end of buffer 
+    bytes_to_eob = (ret_buf->size - relative_byte_offset);
+    //return either upto end of buffer or count, whichever is minimum
+    bytes_to_copy = bytes_to_eob<count?bytes_to_eob:count;
+    //update fpos (partial read rule)
+    *f_pos += bytes_to_copy;
+    PDEBUG("Returng %s\n",ret_buf->buffptr);
+    //copy to user buffer
+    if (copy_to_user(buf, (ret_buf->buffptr + relative_byte_offset), bytes_to_copy)) 
     {
-        bytesToRead = count;
-    }
-    else
-    {
-        bytesToRead = currentReadEntry->size - currentOffset;
-    }
-
-
-    // Copy to user space buffer
-    if(copy_to_user(buf, currentReadEntry->buffptr + currentOffset, bytesToRead))
-    {
-        retval = -EFAULT;
-        goto MUTEX_UNLOCK;
-    }
-
-    // Update the value of f_pos
-    *f_pos += bytesToRead;
-    retval = bytesToRead;
-
-    MUTEX_UNLOCK: mutex_unlock(&(dev->mut));
-    READ_RET: return retval;
+		retval = -EFAULT;
+		//free MUTEX and exit with retval as -EFAULT
+        goto free_lock;
+	}
+    //return number of bytes copied to buffer
+    retval += bytes_to_copy;
+    free_lock: mutex_unlock(&data->lock);
+    ret_func: return retval;
 }
 
 ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
                 loff_t *f_pos)
 {
-    ssize_t retval = -ENOMEM;
-    struct aesd_dev* dev;
-    char* tempPtr;
-
-    PDEBUG("write %zu bytes with offset %lld",count,*f_pos);
-    PDEBUG("Proceed to writing!");
-
-    // Initial checks
-    if(filp == NULL || buf == NULL || f_pos == NULL)
+    ssize_t retval = 0;
+    struct aesd_dev *data;
+    struct aesd_buffer_entry enqueue_buffer;
+    struct working_buffer copy_buffer;
+    int mem_to_malloc,i,start_ptr = 0;
+    char *free_buffer;
+    if(!filp || !filp->private_data)
     {
         retval = -EINVAL;
-        goto WRITE_RET;
+        goto ret_func;
+    }
+    data = filp->private_data;
+
+    PDEBUG("write %zu bytes with offset %lld",count,*f_pos);
+    if(count == 0)
+    {
+        goto ret_func;
     }
 
-    // Fetch the aesd_dev structure
-    dev = filp->private_data;
-
-    // Lock the data using a mutex
-    if(mutex_lock_interruptible(&(dev->mut)))
+    //block on mutex lock, can be interrupted by signal
+    if (mutex_lock_interruptible(&data->lock))
     {
         retval = -EINTR;
-        goto WRITE_RET;
+        
+        //Add a goto statement to the end
+        goto ret_func;
     }
-
-    // Allocate memory for buffer if required
-    if(dev->newWriteEntry.buffptr == NULL)
+    
+    //Allocate normal kernel ram. May sleep.
+    copy_buffer.buffer = kmalloc(count,GFP_KERNEL);
+    if(!copy_buffer.buffer)
     {
-        dev->newWriteEntry.buffptr = kmalloc(count, GFP_KERNEL);
-        if(dev->newWriteEntry.buffptr == NULL)
+        retval = -ENOMEM;
+        
+        //Add a goto statement, need to unlock mutex
+        goto release_lock;
+    }
+    copy_buffer.size = count;
+    
+    //Copy the data from user space buffer into kernel space 
+    if (copy_from_user(copy_buffer.buffer, buf, count)) 
+    {
+		retval = -EFAULT;
+        
+		//Add a goto statement. We need to free COPY_BUFFER and 
+        //UNLOCK MUTEX   
+        goto release_lock;
+	}
+    //WRITE LOGIC
+    //Parse the copy buffer 
+
+    for(i = 0;i<count;i++)
+    {
+        if(copy_buffer.buffer[start_ptr+i]=='\n')
         {
-            goto MUTEX_UNLOCK; // Memory allocation failed
+            //check if working buffer size is 0, if it is malloc
+            //else realloc 
+            mem_to_malloc = (i-start_ptr) +1;
+
+            if(allocate_memory(&data->working_buffer, mem_to_malloc)<0)
+            {
+                retval = -ENOMEM;
+		        //Add a goto statement. We need to free COPY_BUFFER and 
+                //UNLOCK MUTEX   
+                goto copy_buff_free;
+            }
+            //Copy data to global buffer
+            memcpy((data->working_buffer.buffer + data->working_buffer.size),(copy_buffer.buffer+start_ptr),mem_to_malloc);
+            data->working_buffer.size += mem_to_malloc;
+            enqueue_buffer.buffptr = data->working_buffer.buffer;  
+            enqueue_buffer.size = data->working_buffer.size;
+            //enqueue data
+            free_buffer = aesd_circular_buffer_add_entry(&data->circular_buffer,&enqueue_buffer);
+            //if overwrite was performed, free overwrtten buffer
+            if(free_buffer)
+            {
+                kfree(free_buffer);
+            }
+            data->working_buffer.size = 0;
+            //update start pointer in case multiple \n present
+            start_ptr = i+1;
+            retval += mem_to_malloc;
+        }
+        if(i == count - 1)
+        {
+            //incomplete packet transmitted
+            if(copy_buffer.buffer[i] != '\n')
+            {
+                mem_to_malloc = (i-start_ptr) +1;
+                //check if working buffer size is 0, if it is malloc
+                //else realloc 
+                if(allocate_memory(&data->working_buffer, mem_to_malloc)<0)
+                {
+                    retval = -ENOMEM;
+                    
+		            //Add a goto statement. We need to free COPY_BUFFER and 
+                    //UNLOCK MUTEX   
+                    goto copy_buff_free;
+                }
+                //copy data to global buffer and wait for subsequent writes to complete the packet before enqueuing
+                memcpy((data->working_buffer.buffer + data->working_buffer.size),(copy_buffer.buffer+start_ptr),mem_to_malloc);
+                data->working_buffer.size += mem_to_malloc;
+                retval += mem_to_malloc;
+            }
         }
     }
-    else
-    {
-        // Re-allocate the buffer
-        tempPtr = krealloc(dev->newWriteEntry.buffptr, dev->newWriteEntry.size + count, GFP_KERNEL);
-        if(tempPtr == NULL)
-        {
-            kfree(dev->newWriteEntry.buffptr);
-            goto MUTEX_UNLOCK; // Memory allocation failed
-        }
-        else
-        {
-            dev->newWriteEntry.buffptr = tempPtr;
-        }
-    }
-
-    // Copy the contents and update buffer size
-    if(copy_from_user((void*)(dev->newWriteEntry.buffptr + dev->newWriteEntry.size), buf, count))
-    {
-        retval = -EFAULT;
-        goto MUTEX_UNLOCK;
-    }
-    dev->newWriteEntry.size += count;
-
-    // Write to the circular buffer if newline is encountered
-    if(strchr(dev->newWriteEntry.buffptr, '\n') != NULL)
-    {
-        if(dev->circularBuffer.full) // Free existing memory
-        {
-            kfree(dev->circularBuffer.entry[dev->circularBuffer.in_offs].buffptr);
-        }
-        aesd_circular_buffer_add_entry(&(dev->circularBuffer), &(dev->newWriteEntry));
-
-        dev->newWriteEntry.buffptr = NULL;
-        dev->newWriteEntry.size = 0;
-    }
-
-    retval = count;
-
-    MUTEX_UNLOCK: mutex_unlock(&(dev->mut));
-    WRITE_RET: return retval;
+    //free local buffer
+    copy_buff_free: kfree(copy_buffer.buffer);
+    release_lock: mutex_unlock(&data->lock);
+    ret_func: return retval;
 }
 struct file_operations aesd_fops = {
     .owner =    THIS_MODULE,
@@ -226,9 +298,11 @@ int aesd_init_module(void)
         return result;
     }
     memset(&aesd_device,0,sizeof(struct aesd_dev));
-
-    // Initialize the mutex
-    mutex_init(&(aesd_device.mut));
+    //initialize mutex
+    mutex_init(&aesd_device.lock);
+    /**
+     * TODO: initialize the AESD specific portion of the device
+     */
 
     result = aesd_setup_cdev(&aesd_device);
 
@@ -245,9 +319,17 @@ void aesd_cleanup_module(void)
 
     cdev_del(&aesd_device.cdev);
 
-    // Clear up the mutex
-    mutex_destroy(&(aesd_device.mut));
-
+    /**
+     * TODO: cleanup AESD specific poritions here as necessary
+     */
+    //destroy mutex
+    if(mutex_lock_interruptible(&aesd_device.lock))
+    {
+        PDEBUG("MUTEX UNLOCK FAILED");
+    }
+    destroy_circular_buffer(&aesd_device.circular_buffer);
+    mutex_unlock(&aesd_device.lock);
+    mutex_destroy(&aesd_device.lock);
     unregister_chrdev_region(devno, 1);
 }
 
@@ -255,3 +337,4 @@ void aesd_cleanup_module(void)
 
 module_init(aesd_init_module);
 module_exit(aesd_cleanup_module);
+
